04d36784b3c500dcf043b7baeaf29177d9f36964
AliBoustani <ali.boustani.ict@gmail.com>
2018-11-10 11:23:36 +0330

    VIP Group Creation Added

--- VIP/vip_generator.py
+++ VIP/vip_generator.py
@@ -29,17 +29,26 @@ My_fortigate = {
     }
 
 
+#Establishing SSH connection
+net_connect = ConnectHandler(**My_fortigate)
+
 #The function!
 def fg_vip_conf(vip_name, intip, extip, port):
 
-    commands = ['config firewall vip', 'edit '+ vip_name+port,
+    #VIP script
+    commands_vip = ['config firewall vip', 'edit '+ vip_name+port,
 'set extip '+ extip ,'set extintf wan1','set mappedip '+ intip,'set portforward enable','set protocol tcp','set extport '+port,'set mappedport '+port, 'end']
+    
+    #VIP Group script
+    commands_vipgrp = ['config firewall vipgrp', 'edit '+ vip_name,'set interface wan1', 'append member ' + vip_name+port, 'end']
 
-    net_connect = ConnectHandler(**My_fortigate)
+    output_vip = net_connect.send_config_set(commands_vip)
+    output_vipgrp = net_connect.send_config_set(commands_vipgrp)
 
-    output = net_connect.send_config_set(commands)
 
-   # print(output)
+   # print(output_vip)
+   # print(output_vipgrp)
+
 
 
 
@@ -47,6 +56,6 @@ def fg_vip_conf(vip_name, intip, extip, port):
 #It starts from second raw! First raw is just about each column informations.
 for j in range (2, maxrow+1):
     dic1 = dict((i, str(ws.cell(row=j, column=i).value)) for i in range(1, maxcols+1))
-    for k in range(4, maxcols+1):  #Considering that the specified ports start at column 4 !
+    for k in range(4, maxcols+1):  #Considering the specified ports start at column 4 !
       if dic1[k] != 'None':
           fg_vip_conf(dic1[1], dic1[2], dic1[3], dic1[k])
