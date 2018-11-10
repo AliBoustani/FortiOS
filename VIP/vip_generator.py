fbc0c7a2981656910903415589565f7a965664a5
AliBoustani <ali.boustani.ict@gmail.com>
2018-11-10 09:47:29 +0330

    Some fixes

--- VIP/vip_generator.py
+++ VIP/vip_generator.py
@@ -2,9 +2,9 @@ from openpyxl import load_workbook
 from netmiko import ConnectHandler
 import getpass
 
-password = getpass.getpass()
 
-#Specify your excel document
+
+#Specify your excel document! This is my test sample
 wb = load_workbook(filename='MyDoc.xlsx')
 
 #The sheet name in your excel file
@@ -14,13 +14,17 @@ ws = wb['Sheet1']
 maxrow = ws.max_row
 maxcols = ws.max_column
 
-#Your device IP address
+#Enter your FortiGate IP address and credentials
 host = raw_input("Please Enter your FortiGate device IP address: ")
+username = raw_input("Please Enter your FortiGate username: ")
+password = getpass.getpass()
+
+
 #Creating connection profile for your device
-fortigate_200e = {
+My_fortigate = {
     'device_type': 'fortinet',
     'ip': host,
-    'username': 'admin',
+    'username': username,
     'password': password,
     }
 
@@ -31,7 +35,7 @@ def fg_vip_conf(vip_name, intip, extip, port):
     commands = ['config firewall vip', 'edit '+ vip_name+port,
 'set extip '+ extip ,'set extintf wan1','set mappedip '+ intip,'set portforward enable','set protocol tcp','set extport '+port,'set mappedport '+port, 'end']
 
-    net_connect = ConnectHandler(**fortigate_200e)
+    net_connect = ConnectHandler(**My_fortigate)
 
     output = net_connect.send_config_set(commands)
 
@@ -40,13 +44,9 @@ def fg_vip_conf(vip_name, intip, extip, port):
 
 
 #Applying th function for each column in each row
+#It starts from second raw! First raw is just about each column informations.
 for j in range (2, maxrow+1):
     dic1 = dict((i, str(ws.cell(row=j, column=i).value)) for i in range(1, maxcols+1))
-    for k in range(4, maxcols+1):
+    for k in range(4, maxcols+1):  #Considering that the specified ports start at column 4 !
       if dic1[k] != 'None':
           fg_vip_conf(dic1[1], dic1[2], dic1[3], dic1[k])
-
-
-
-
-
